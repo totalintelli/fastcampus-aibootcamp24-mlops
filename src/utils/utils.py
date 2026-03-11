0ed26551fee673ab5f9ee362fede6c8c06d3ad45
totalintelli <blueintelli@gmail.com>
2026-03-11 07:40:20 +0000

    feat : Create utils.py
    유틸리티성 코드를 모듈화하여 기능을 분리.

---
+++ src/utils/utils.py
@@ -0,0 +1,35 @@
+'''
+파일 명칭 : utils.py 
+기     능 : 유틸리티성 코드 모듈화
+입     력 : 없음
+출     력 : 없음
+작 성  자 : 송 용 단
+작성 일자 : 2026-03-11
+'''
+import os
+import random
+
+import numpy as np
+
+
+def init_seed():
+    np.random.seed(0)
+    random.seed(0)
+
+
+def project_path():
+    return os.path.join(
+        os.path.dirname(
+            os.path.abspath(__file__)
+        ),
+        "..",
+        ".."
+    )
+
+
+def model_dir(model_name):
+    return os.path.join(
+        project_path(),
+        "models",
+        model_name
+    )
