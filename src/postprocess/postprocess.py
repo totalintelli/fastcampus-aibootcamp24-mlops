52a030c1d5befc5490804c0b8f179cbdaec25e85
totalintelli <blueintelli@gmail.com>
2026-03-12 06:10:05 +0000

    feat : 배치 데이터 가져오기

--- src/postprocess/postprocess.py
+++ src/postprocess/postprocess.py
@@ -10,7 +10,39 @@
 import os
 
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
+
+
+def get_engine(db_name):
+    engine = create_engine(
+        url=(
+            f"mysql+mysqldb://"
+            f"{os.environ.get('DB_USER')}:"
+            f"{os.environ.get('DB_PASSWORD')}@"
+            f"{os.environ.get('DB_HOST')}:"
+            f"{os.environ.get('DB_PORT')}/"
+            f"{db_name}"
+        )
+    )
+    return engine
+
+
+# write_db 에서도 get_engine 사용하도록 수정
+
+
+def read_db(db_name, table_name, k=10):
+    engine = get_engine(db_name)
+    connect = engine.connect()
+    result = connect.execute(
+        statement=text(
+            f"select recommend_content_id from {table_name} "
+            f"order by `index` desc limit :k"
+        ),
+        parameters={"table_name": table_name, "k": k},
+    )
+    connect.close()
+    contents = [data[0] for data in result]
+    return contents
 
 
 def write_db(data: pd.DataFrame, db_name, table_name):
